9d175e67d7ec7256cfd4b1fd9cbc1af70ab6620e
srinivasaleti <srinivas.aleti03@gmail.com>
2025-03-10 19:55:43 +0530

    🚀🔧 Add GitHub Actions workflow for Catalog Service CI
    - ➕ Added a GitHub Actions workflow to build and test `catalog-service`
    - 🛠️  Uses Maven Wrapper (`./mvnw`) for consistent builds
    - 🎯 Runs only when changes are made in `catalog-service/`
    
    - ✅ Ensures automated CI without unnecessary runs for unrelated changes
    - 🔄 Guarantees the correct Maven version using Maven Wrapper
    - ⏩ Improves build speed with caching and optimized setup

--- catalog-service/src/test/java/com/srinivas/bookstore/catalog/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/srinivas/bookstore/catalog/CatalogServiceApplicationTests.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
+import static org.junit.Assert.assertEquals;
+
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class CatalogServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1, 1);
 	}
 
 }
