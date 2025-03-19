a0cff18cb8ddf5ec9569526613158c18282d6e26
srinivasaleti <srinivas.aleti03@gmail.com>
2025-03-19 14:58:46 +0530

    ✨ Add integration test setup & RestAssured config
    ✅ Introduced AbstractIntegrationTest to avoid duplication in test classes.
    🔹 Configures RestAssured with dynamic port from Spring Boot.
    🔹 Uses @SpringBootTest with RANDOM_PORT to support dynamic testing.
    🔹 Ensures all integration tests inherit common setup.
    
    🎯 This setup ensures maintainability and avoids redundant test configs.

--- catalog-service/src/test/java/com/srinivas/bookstore/catalog/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/srinivas/bookstore/catalog/CatalogServiceApplicationTests.java
@@ -1,18 +1,15 @@
 package com.srinivas.bookstore.catalog;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 import static org.junit.Assert.assertEquals;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
-class CatalogServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-		assertEquals(1, 1);
-	}
+class CatalogServiceApplicationTests extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {
+        assertEquals(1, 1);
+    }
 
 }
