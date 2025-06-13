9d181c5ce0e7fb7730010e271a21cbafa687f359
srinivasaleti <srinivas.aleti03@gmail.com>
2025-06-13 15:03:53 +0530

    feat(k8s): add catalog-db deployment with persistent storage
    - Added Deployment for catalog-db using postgres:16-alpine
    - Configured environment variables (user, password, db)
    - Exposed catalog-db via ClusterIP service for internal access
    - Mounted PersistentVolumeClaim to /var/lib/postgresql/data
    - Volume backed by hostPath at /tmp/k8s/catalog-db-data in Minikube

--- tools/make/app.mk
+++ tools/make/app.mk
@@ -1,3 +1,6 @@
 install: ui-install  ## Install all packages
 	yarn
 
+k8s-up: catalog-db-k8s-deploy ## Deploy  k8s
+
+k8s-down: catalog-db-k8s-destroy ## Deploy  k8s
--- tools/make/catalog-service.mk
+++ tools/make/catalog-service.mk
@@ -7,3 +7,8 @@ catalog-service-build:  ## Build JAR and Docker image
 	cd $(CATALOG_DIR) && mvn clean package
 	cd $(CATALOG_DIR) && docker buildx build --platform linux/amd64 -t catalog-service -f Dockerfile .
 
+catalog-db-k8s-deploy: ## Deploy k8s catalog db
+	kubectl apply -f deployments/k8s/catalog-db
+
+catalog-db-k8s-destroy: ## Deploy k8s catalog destroy.
+	kubectl delete -f deployments/k8s/catalog-db
