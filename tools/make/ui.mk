87446f606e65e2f3676ba42c6ef62fa239e7b462
srinivasaleti <srinivas.aleti03@gmail.com>
2025-06-16 15:49:35 +0530

    Deploy UI to k8s

--- tools/make/ui.mk
+++ tools/make/ui.mk
@@ -6,3 +6,9 @@ UI_DIR=ui
 .PHONY: ui-build  ## Build UI docker image
 ui-build:  ## Build UI Docker image
 	cd $(UI_DIR) && docker buildx build --platform linux/amd64 -t bookstore-ui -f Dockerfile .
+
+ui-k8s-deploy:
+	kubectl apply -f deployments/k8s/ui/
+
+ui-k8s-destroy:
+	kubectl delete -f deployments/k8s/ui/
